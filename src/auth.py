b8fc539b8bc966d15cdd15b05b04509077db9ffb
NickGolovanov <nikita.golovanov@student.nhlstenden.com>
2025-02-25 15:53:01 +0100

    adjustes the query

--- src/auth.py
+++ src/auth.py
@@ -19,24 +19,35 @@ access_secret = os.environ.get("TWITTER_ACCESS_SECRET_TOKEN")
 # )
 
 
-client = tweepy.Client(bearer_token, consumer_key, consumer_secret, access_token, access_secret)
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_secret,
+    wait_on_rate_limit=True
+)
 
 # api = tweepy.API(auth, wait_on_rate_limit=True)
 
-search_query = "Elon Musk fired -filter:retweets AND -filter:replies AND -filter:links"
+search_query = "Elon Musk fired -is:retweet -is:reply -has:links"
 # search_query = "point_radius:[4.895168 53.370216 25km]"
 no_of_tweets = 100
 
 try:
-    #The number of tweets we want to retrieved from the search
-    # tweets = api.search_tweets(q=search_query, lang="en", count=no_of_tweets, tweet_mode ='extended')
-    
-    tweets = client.search_recent_tweets(query=search_query, max_results=no_of_tweets)
+    # Fetch tweets
+    tweets = client.search_recent_tweets(query=search_query, max_results=no_of_tweets, tweet_fields=["created_at", "author_id"])
 
-    attributes_container = [[tweet.user.name, tweet.created_at, tweet.favorite_count, tweet.source, tweet.full_text] for tweet in tweets]
+    # Process and store tweets
+    if tweets.data:
+        attributes_container = [[tweet.author_id, tweet.created_at, tweet.text] for tweet in tweets.data]
 
-    columns = ["User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet"]
-    
-    tweets_df = pd.DataFrame(attributes_container, columns=columns)
-except BaseException as e:
-    print('Status Failed On,',str(e))
+        columns = ["User ID", "Date Created", "Tweet"]
+        tweets_df = pd.DataFrame(attributes_container, columns=columns)
+
+        print(tweets_df)
+    else:
+        print("No tweets found.")
+
+except Exception as e:
+    print("❌ Status Failed On:", str(e))
